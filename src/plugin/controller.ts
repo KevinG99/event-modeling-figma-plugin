2f4247b6522f8331ac7d1f4b26f5adfd037ca163
Kevin Grote <kevin.grote.ltd@gmail.com>
2024-03-15 23:42:28 +0200

    safe state

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,24 +1,43 @@
-import { createCommandSection, createEventStickyNote, getOrangeStickies, moveStickyToSection  } from '../app/methods/helper';
-import { EventMessage, ActionTypes, UIAction } from '../app/types';
+import {
+  createCommandSection,
+  createEventStickyNote,
+  getOrangeStickies,
+  moveStickyToSection,
+} from '../app/methods/helper';
+import { EventMessage, ActionTypes, UIAction, BulkEventMessage } from '../app/types';
+
 figma.showUI(__html__);
 
 export const ORANGE_COLOR = { r: 1, g: 0.647, b: 0 };
 export const BLUE_COLOR = { r: 0.529, g: 0.808, b: 0.922 };
 export const COLOR_TOLERANCE = 0.01;
-export const CENTER_X = figma.viewport.center.x;
-
-export const CENTER_Y = figma.viewport.center.y;
 
 figma.ui.resize(300, 600);
 
-figma.ui.onmessage = async (msg: EventMessage | UIAction) => {
-  if (msg.type === ActionTypes.CreateEventStickyNote) {
-    await createEventStickyNote(<EventMessage>msg).then((sticky) => moveStickyToSection(sticky));
-  } else if (msg.type === ActionTypes.CreateCommandStickyNote) {
-    await getOrangeStickies().then((stickies) => {
-      stickies.forEach((sticky) => createCommandSection(sticky));
-    });
+figma.ui.onmessage = async (msg: EventMessage | UIAction | BulkEventMessage) => {
+  switch (msg.type) {
+    case ActionTypes.CreateEventStickyNote:
+      await createEventStickyNote(<EventMessage>msg).then((sticky) => {
+        const sectionNode = moveStickyToSection(sticky);
+        figma.viewport.scrollAndZoomIntoView([sectionNode]);
+      });
+      break;
+    case ActionTypes.CreateCommandStickyNote:
+      await getOrangeStickies().then((stickies) => {
+        stickies.forEach((sticky) => createCommandSection(sticky));
+      });
+      break;
+    case ActionTypes.CreateBulkEvents:
+      const nodes = [];
+      (msg as BulkEventMessage).eventNames.forEach((eventName) => {
+        createEventStickyNote({ type: ActionTypes.CreateEventStickyNote, eventName, properties: [] }).then((sticky) => {
+            const sectionNode = moveStickyToSection(sticky);
+            nodes.push(sectionNode);
+            figma.currentPage.appendChild(sectionNode);
+          },
+        );
+      });
+      figma.viewport.scrollAndZoomIntoView(nodes);
+      break;
   }
 };
-
-
